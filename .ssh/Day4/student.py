9a75a340ffc29c7371d3bc201047446f91bb6853
gregorule <gregorule8@gmail.com>
2022-06-09 11:29:27 +0000

    student update commit

--- .ssh/Day4/student.py
+++ .ssh/Day4/student.py
@@ -1,10 +1,19 @@
 class Student:
 
-    def __init__(self, name, age):
+    def __init__(self, name = "Student", age = "Student", cohert = "Student"):
         self.name = name
         self.age = age
+        self.cohert = cohert
 
-John = Student("John", "21")
-Jane = Student("Jane", "22")
+    def final(self, test1, test2, test3):
+        avg = (test1 + test2 + test3)/3
+        return f"{self.name} your average score is: {avg}"
+
+John = Student("John", "21", "22AprEnable3")
+Jane = Student("Jane", "22", "22AprEnable3")
 
 print(getattr(John, "age"))
+print(getattr(Jane, "cohert"))
+print(John.final(15,25,30))
+
+
