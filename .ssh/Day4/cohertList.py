a23ac9111ee0c12a972015317f317459b2f82087
gregorule <gregorule8@gmail.com>
2022-06-09 09:42:10 +0000

    coherList and factorial commit

---
+++ .ssh/Day4/cohertList.py
@@ -0,0 +1,6 @@
+cohertNames = ["Chris", "Henry", "Immy", "Greg", "Gregor", "Dwayne", "Elliot"]
+cohertNames.append("Ryan")
+print(cohertNames)
+print(cohertNames[4])
+
+print(cohertNames.count("Chris"))
---
+++ .ssh/Day4/fact.py
@@ -0,0 +1,7 @@
+def fact(x):
+    count = 1
+    for x in range(1, x+ 1):
+        count *= x
+    return count
+
+print(fact(5))
